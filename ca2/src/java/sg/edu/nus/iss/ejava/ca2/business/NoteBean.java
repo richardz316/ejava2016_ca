3b9193f84fd1b6b4cbdbc73da1d3be3e51a81286
richardz316 <jsman316@gmail.com>
2016-11-05 21:07:11 +0800

    -create review for Create Note

--- ca2/src/java/sg/edu/nus/iss/ejava/ca2/business/NoteBean.java
+++ ca2/src/java/sg/edu/nus/iss/ejava/ca2/business/NoteBean.java
@@ -5,10 +5,34 @@
  */
 package sg.edu.nus.iss.ejava.ca2.business;
 
+import java.util.Date;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sg.edu.nus.iss.ejava.ca2.model.Notes;
+
 /**
  *
  * @author rzhao
  */
 public class NoteBean {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Resource SessionContext ctx;
+    
+    public void add(String userid, String title, String category, String content, Date postdate){
+        
+        Notes notes = new Notes();
+        notes.setUserid(userid);
+        notes.setTitle(title);
+        notes.setCategory(category);
+        notes.setContent(content);
+        notes.setPostdate(postdate);
+        
+        em.persist(notes);
+    }
     
 }
