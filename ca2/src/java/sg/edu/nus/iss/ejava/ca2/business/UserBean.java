c07f8f6af636d685d25dff6039ccd35da90216d4
richardz316 <jsman316@gmail.com>
2016-11-05 17:04:39 +0800

    -create loginView

--- ca2/src/java/sg/edu/nus/iss/ejava/ca2/business/UserBean.java
+++ ca2/src/java/sg/edu/nus/iss/ejava/ca2/business/UserBean.java
@@ -5,10 +5,28 @@
  */
 package sg.edu.nus.iss.ejava.ca2.business;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import sg.edu.nus.iss.ejava.ca2.model.Users;
+
 /**
  *
  * @author rzhao
  */
 public class UserBean {
+    @PersistenceContext
+    private EntityManager em;
     
+    public void add(String name, String email) {
+        Users users = new Users();
+//        user.
+        
+//        em.persist(user);
+    }
+    
+
 }
