d0f2fa96a339aedd1c40d16ad110d8ec647f94e4
E0015387 <E0015387@ISSCR2120.iss.nus.edu.sg>
2016-11-12 15:25:17 +0800

    send and callback link

---
+++ ca3/src/java/epod/rest/EPodForwardHqTask.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epod.rest;
+
+import epod.model.Pod;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.media.multipart.BodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
+/**
+ *
+ * @author E0015387
+ */
+public class EPodForwardHqTask implements Runnable {
+    
+    private Pod pod;
+    
+    public void setPodData(Pod pod){
+        this.pod = pod;
+    }
+    
+    @Override
+    public void run() {
+        
+        System.out.println("--> Starting FetchItemTask Thread ");
+        
+        Client client = ClientBuilder.newBuilder().register(MultiPartFeature.class).build();
+
+	byte[] buffer = pod.getImage();
+	BodyPart imgPart = new BodyPart(buffer, MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        
+        imgPart.setContentDisposition(
+			FormDataContentDisposition.name("image")
+			.fileName("ca3.png").build());
+
+	//Add other fields
+	MultiPart formData = new FormDataMultiPart()
+			.field("teamId", "41b26e1c", MediaType.TEXT_PLAIN_TYPE)
+			.field("podId", pod.getPodId(), MediaType.TEXT_PLAIN_TYPE)
+                        .field("callback", "http://192.168.56.1:8080/ca3/callback", MediaType.TEXT_PLAIN_TYPE)
+                        .field("note", pod.getNote(), MediaType.TEXT_PLAIN_TYPE)
+			.bodyPart(imgPart);
+	formData.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+
+	WebTarget target = client.target("http://10.10.0.48:8080/epod/upload");
+	Invocation.Builder inv = target.request();
+
+	Response callResp = inv.post(Entity.entity(formData, formData.getMediaType()));
+
+	System.out.println(">> call resp:" + callResp.getStatus());
+
+    }
+    
+    
+    
+}
---
+++ ca3/test/epod/rest/EPodForwardHqTaskTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epod.rest;
+
+import epod.model.Delivery;
+import epod.model.Pod;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.junit.Test;
+
+/**
+ *
+ * @author E0015387
+ */
+public class EPodForwardHqTaskTest {
+    
+    @Test
+    public void testUpload() throws IOException {
+	EPodForwardHqTask task = new EPodForwardHqTask();
+	Delivery deliver = new Delivery();
+	deliver.setPkgId(1);
+	deliver.setName("Tester");
+	deliver.setAddress("Test Address");
+	
+	Pod pod = new Pod();
+	pod.setDelivery(deliver);
+	pod.setPodId(1);
+	pod.setNote("Testing123");
+        
+        File fnew=new File("/tmp/rose.jpg");
+        BufferedImage originalImage=ImageIO.read(fnew);
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        ImageIO.write(originalImage, "jpg", baos );
+        byte[] imageInByte=baos.toByteArray();
+        
+        pod.setImage(imageInByte);
+
+	task.setPodData(pod);
+	task.run();
+	
+    }
+}
