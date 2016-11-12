e9f049b4f133ce0476a4738d9626c6571eb9088a
E0015387 <E0015387@ISSCR2120.iss.nus.edu.sg>
2016-11-12 09:32:21 +0800

    entity

--- ca3/src/java/epod/model/Delivery.java
+++ ca3/src/java/epod/model/Delivery.java
@@ -5,10 +5,28 @@
  */
 package epod.model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 /**
  *
  * @author E0015387
  */
-public class Delivery {
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Delivery.findAll", query = "SELECT d FROM Delivery d")})
+public class Delivery implements Serializable {
     
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Column(name = "pkg_id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int pkgId;
 }
